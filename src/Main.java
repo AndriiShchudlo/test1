e3df3c0134ea0cc287fc82f16640a1d484be3638
ShAndriy <andriyshchudlo@gmail.com>
2016-11-28 17:17:08 +0200

    firstDayOfMonth

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
-import java.util.IntSummaryStatistics;
-
+import  java.time.LocalDate;
+import  java.time.*;
+import static java.time.temporal.TemporalAdjusters.*;
 /**
  * Created by andriis on 11/28/16.
  */
 public class Main {
     String name = "";
     int age = 0;
+    private static LocalDate date;
+
+
     public static void main(String[] args){
+        date  = LocalDate.now();
+
+        System.out.println(date.with(firstDayOfMonth()).getDayOfWeek().getValue());
         Main person = new Main();
         person.name = "Andriy";
         person.age = 23;
