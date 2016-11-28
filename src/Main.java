f47b9b65556c1216bfa72b501d9df7dcd6af5862
ShAndriy <andriyshchudlo@gmail.com>
2016-11-28 15:59:53 +0200

    Add person Andriy

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.IntSummaryStatistics;
+
 /**
  * Created by andriis on 11/28/16.
  */
 public class Main {
+    String name = "";
+    int age = 0;
     public static void main(String[] args){
-
-        System.out.println("Hello");
+        Main person = new Main();
+        person.name = "Andriy";
+        person.age = 23;
+        System.out.println("Name: " + person.name + " Age: " + person.age);
     }
 }
